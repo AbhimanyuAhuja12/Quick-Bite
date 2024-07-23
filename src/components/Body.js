7b41c57966c8a1b940872c64d77813a0e754f1d1
Abhimanyu Ahuja <abhimanyuahuja12@gmail.com>
2024-07-24 01:31:21 +0530

    added tailwind config and remove standard css components

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,27 +1,31 @@
-import ResturantCard from "./ResturantCard";
+import ResturantCard,{withPromotedLabel} from "./ResturantCard";
 import { useState ,useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
-import useOnlineStatus from "../utils/useOnlineStatus.js"
-// import useCarouselData from "../utils/useCarouselData";
+import useOnlineStatus from "../utils/useOnlineStatus"
+
 
 
 
 
 
 const Body =()=>{
+  // Local state variable - Super Powerful variable
   const [listOfResturants, setlistOfResturant]= useState([]);
   const [filteredResturant , setfilteredResturant] =useState([]);
 
   const [ searchText , setsearchText] =useState("");
 
-// const [json] = useCarouselData();
+
+  const RestauranCardPromoted =withPromotedLabel(ResturantCard);
+
 
 useEffect(()=>{
  fetchData();
 }, []);
 
  const fetchData =async()=>{
+  
   const data =await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6578133&lng=77.28181959999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
   
 
@@ -31,8 +35,8 @@ useEffect(()=>{
 
   setlistOfResturant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
   setfilteredResturant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
- 
- }
+ };
+
 
 
 
@@ -40,12 +44,13 @@ const OnlineStatus=useOnlineStatus();
 if(OnlineStatus===false) return (<h1>Looks like you are offline!! please check your internet connection</h1>);
 
     return listOfResturants.length===0 ? <Shimmer /> : (
-       <div className="body">
+       <div className="body  ">
 
-       <div className="search">
-        <input className="search-bar" type="text" 
+      <div className="flex">
+       <div className=" flex search m-4  p-4 f fa-align-justify">
+        <input className="search-bar border:border-solid" type="text" 
         value={searchText} onChange={(e)=>{setsearchText(e.target.value)}} placeholder="search here" />
-        <button id="submit" onClick={()=>{
+        <button  className="px-4 py-2 bg-green-500 rounded-lg" id="submit" onClick={()=>{
           // filter logic
          
 
@@ -58,11 +63,11 @@ if(OnlineStatus===false) return (<h1>Looks like you are offline!! please check y
          >Search</button> 
        </div>
        
-       <div className="filter">
-        <button className="filter-btn" onClick={()=>{
+       <div className="filter flex items-center">
+        <button className="filter-btn bg-gray-300 rounded-lg w-48" onClick={()=>{
           //filter logic here
           const filteredList =listOfResturants.filter((res)=> res.info.avgRating>4.2);
-          // console.log(filteredList);
+          
           setlistOfResturant(filteredList);
          
 
@@ -70,12 +75,16 @@ if(OnlineStatus===false) return (<h1>Looks like you are offline!! please check y
         Top Rated Resturant
            </button>
        </div>
+       </div>
 
 
-       <div className="resturant-container">
+       <div className="resturant-container flex flex-wrap justify-center gap-4">
       
        {filteredResturant.map((resturant) => (
-        <Link key={resturant.info.id}  to={"/restaurant/" + resturant.info.id}><ResturantCard resData={resturant} /> </Link>
+        <Link key={resturant.info.id}  to={"/restaurant/" + resturant.info.id}>
+          {/* if restaurant is promoted  then add promoted label to it*/}
+          {resturant.info.promoted ? <RestauranCardPromoted resData={resturant}/>:
+          <ResturantCard resData={resturant} />} </Link>
         ))};
       
          {/* console.log(resList.get(0)); */}
@@ -87,6 +96,5 @@ if(OnlineStatus===false) return (<h1>Looks like you are offline!! please check y
        </div>
     );
 };
-
 
 export default Body;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,41 +3,38 @@ import { LOGO_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
 
-const Header =()=>{
+const Header = () => {
+  const [btnNameReact, setBtnNameReact] = useState("Login");
+  const onlineStatus = useOnlineStatus();
 
-// let btnName = "Login";.
-const [btnNameReact, setbtnNameReact] = useState("Login")
+  return (
+    <div className="Header flex justify-between border border-gray-400 p-4 l">
+      {/* Logo of the website */}
+      <div className="logo-container">
+        <img className="logo w-24" src={LOGO_URL} alt="Website Logo" />
+      </div>
 
-const InternetStatus =useOnlineStatus();
-
-    return(
-        <div className="header">
-         
-         {/* logo of website */}
-          <div className="log-container">
-            <img className="logo" src={LOGO_URL}>
-
-            </img>
-          </div >
-         
-         {/* menu items */}
-          <div className="nav-items">
-            <ul>
-              <li>Online Status:{InternetStatus ? "✅":"🔴"}</li>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">AboutUs</Link></li>
-                <li><Link to="/contact">ContactUs</Link></li>
-                <li>Cart</li>
-                <button className="login-button" onClick={()=>{
-                 btnNameReact==="Login" ? setbtnNameReact("Logout") : setbtnNameReact("Login")
-                  // console.log(btnName);
-                }}>☢️{btnNameReact}</button>
-            </ul>
-          </div>
-        
-
-        </div>
-    );
+      {/* Menu items */}
+      <div className="nav-items">
+        <ul className="flex list-none text-2xl">
+          <li className="p-2 m-2">Online Status: {onlineStatus ? "✅" : "🔴"}</li>
+          <li className="p-2 m-2"><Link to="/" className="no-underline">Home</Link></li>
+          <li className="p-2 m-2"><Link to="/about" className="no-underline">About Us</Link></li>
+          <li className="p-2 m-2"><Link to="/contact" className="no-underline">Contact Us</Link></li>
+          <li className="p-2 m-2">Cart</li>
+          <li className="p-2 m-2 "><Link to="/grocery" className="no-underline">Grocery</Link></li>
+          <button 
+            className="login-button p-2 m-2 bg-white border-none cursor-pointer"
+            onClick={() => {
+              setBtnNameReact(btnNameReact === "Login" ? "Logout" : "Login");
+            }}
+          >
+            ☢️{btnNameReact}
+          </button >
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
